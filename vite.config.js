a7e9f37a7f4363461f438e2cb4dccd4de82ee791
Sharad <srd28nov21@gmail.com>
2025-05-17 15:26:32 +0530

    Changes

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/softsell/', // <-- match your repo name
+  base: '/', // <-- match your repo name
 });
